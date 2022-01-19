fe4a971f917ebb4576de39960053814b37e6c36a
sychsergiy <sychsergiy@gmail.com>
2022-01-19 22:29:23 +0200

    add sort task

---
+++ simple_algorithms/sort.py
@@ -0,0 +1,31 @@
+# https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%BF%D1%83%D0%B7%D1%8B%D1%80%D1%8C%D0%BA%D0%BE%D0%BC
+def bubble_sort(sequence):
+    pass
+
+
+def test_bubble_sort():
+    assert bubble_sort([25, 76, 89, 95, 84, 4, 11, 66, 43, 38, 73, 100, 64, 90, 17]) == [
+        4, 11, 17, 25, 38, 43, 64, 66, 73, 76, 84, 89, 90, 95, 100
+    ]
+
+
+# https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%B2%D1%81%D1%82%D0%B0%D0%B2%D0%BA%D0%B0%D0%BC%D0%B8
+def insertion_sort(sequence):
+    pass
+
+
+def test_insertion_sort():
+    assert insertion_sort([25, 76, 89, 95, 84, 4, 11, 66, 43, 38, 73, 100, 64, 90, 17]) == [
+        4, 11, 17, 25, 38, 43, 64, 66, 73, 76, 84, 89, 90, 95, 100
+    ]
+
+
+# https://ru.wikipedia.org/wiki/%D0%91%D1%8B%D1%81%D1%82%D1%80%D0%B0%D1%8F_%D1%81%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0
+def quick_sort(sequence):
+    pass
+
+
+def test_quick_sort():
+    assert quick_sort([25, 76, 89, 95, 84, 4, 11, 66, 43, 38, 73, 100, 64, 90, 17]) == [
+        4, 11, 17, 25, 38, 43, 64, 66, 73, 76, 84, 89, 90, 95, 100
+    ]
